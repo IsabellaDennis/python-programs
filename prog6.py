c767c64ae5b1f076f6bde109d75c5719e94ad652
IsabellaDennis <160301294+IsabellaDennis@users.noreply.github.com>
2025-10-16 20:38:44 +0530

    Update prog6.py

--- prog6.py
+++ prog6.py
@@ -3,9 +3,13 @@
 
 
 
-nums = list(map(int, input("Enter numbers: ").split()))
-print(list(set(nums)))
+def unique_words_list(words):
+    unique_words = []
+    [unique_words.append(word) for word in words if word not in unique_words]
+    return unique_words
 
+words = input("Enter words separated by space: ").split()
+print("Unique words:", unique_words_list(words))
 
 # Enter numbers: 1 2 2 3 4 4 5
 # [1, 2, 3, 4, 5]
